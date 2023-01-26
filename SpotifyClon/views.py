a731e4cc80ebd11964174e46c6005e85832712e1
Andres S <pipedj12@hotmail.es>
2023-01-26 14:16:53 -0500

    se creo una vista con parametros y otra sin parametros

---
+++ SpotifyClon/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+#inciando vistas
+def index(request):
+    """primer saludo en django!"""
+    return HttpResponse("<h3> Hola mundo!! </h3>")
+
+#vista con parametros
+def calcular_mayoria_edad(request, edad: int) -> str:
+    if edad >= 18:
+        mensaje = "es mayor de edad"
+    else:
+        mensaje = "es menor de edad!"
+
+    return HttpResponse(f"<h3>{mensaje}</h3>") 
